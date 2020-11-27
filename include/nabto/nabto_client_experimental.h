a41975fac17621ec172d22351c3faa8443d5df89
fusionfoxy <thom2287@gmail.com>
2020-11-27 07:54:55 +0100

    nabto client libs and 3. party headers

---
+++ include/nabto/nabto_client_experimental.h
@@ -0,0 +1,48 @@
+#ifndef NABTO_CLIENT_EXPERIMENTAL_H
+#define NABTO_CLIENT_EXPERIMENTAL_H
+
+#include "nabto_client.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * Experimental header. Functions here are all experimental. They
+ * should be used with caution and can be changed in future releases
+ * without notice.
+ */
+
+
+
+/**
+ * TCP Tunel experimental features
+ */
+
+
+/* enum NabtoClientTcpTunnelListenMode { */
+/*     LISTEN_MODE_LOCALHOST, */
+/*     LISTEN_MODE_ANY */
+/* }; */
+
+/**
+ * NOT IMPLEMENTED. TBD.
+ * Set the listen mode for the tcp listener. Default is to only listen
+ * on localhost / loopback such that only applications on the local
+ * machine can connect to the tcp listener. Anyone on the local system
+ * can connect to the tcp listener. Some form of application layer
+ * authentication needs to be present on the tcp connection if the
+ * system is multi tenant or not completely trusted or if the
+ * application is not run in isolation.
+ */
+/* NABTO_CLIENT_DECL_PREFIX NabtoClientError NABTO_CLIENT_API */
+/* nabto_client_tcp_tunnel_listen_mode(NabtoClientTcpTunnel* tunnel, */
+/*                                     enum NabtoClientTcpTunnelListenMode listenMode); */
+
+
+
+#ifdef __cplusplus
+} // extern C
+#endif
+
+#endif
