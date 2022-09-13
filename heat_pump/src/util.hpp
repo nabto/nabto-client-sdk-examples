025796525b9536e11b57bc2062a892ac38345a70
fusionfoxy <thom2287@gmail.com>
2022-09-13 13:41:39 +0200

    heat pump example removed as it is replaced with thermostat

--- heat_pump/src/util.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include <nabto/nabto_client.h>
-#include <string>
-
-std::string time_in_HH_MM_SS_MMM();
-
-void handle_connect_error(NabtoClientConnection* connection, NabtoClientError ec);
