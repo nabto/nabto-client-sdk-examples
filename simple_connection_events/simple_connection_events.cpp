4d4fd79ddb06e479223893119eb980aefb6158b5
Ulrik Gammelby <ug@nabto.com>
2024-06-09 16:19:34 +0200

    add docs reference

--- simple_connection_events/simple_connection_events.cpp
+++ simple_connection_events/simple_connection_events.cpp
@@ -68,7 +68,7 @@ void init_connection_event_listener(NabtoClient* context, NabtoClientConnection*
 
 void arm_connection_event_listener(NabtoClientConnection* connection) {
     // initialize future for the event listener, the future is resolved when a new connection event is
-    // ready or the listener has been stopped
+    // ready or the listener has been stopped (also see https://docs.nabto.com/developer/platforms/embedded/nabto_futures.html#listeners)
     nabto_client_listener_connection_event(listener_, eventFuture_, &connectionEvent_);
     nabto_client_future_set_callback(eventFuture_, connection_event_cb, connection);
 }
